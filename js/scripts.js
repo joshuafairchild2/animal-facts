6bb28bebdb8de48b1a480401a32f44afddaa29b4
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-02-12 03:07:54 -0800

    add javascript logic to make each radio button act as a selector for which animal the user would like to learn about, as well as basic styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 $(function() {
-  $("input[name=animals-button]:radio").click(function() {
+    // $('.turtles').hide();
+    // $('.snakes').hide();
+    // $('.insects').hide();
 
-    if ($("input[name=animals-button]:checked").val() == "Turtles") {
-      alert("turtles");
-  }
+    $("input[name=animals-button]:radio").click(function () {
+        if ($('input[name=animals-button]:checked').val() == "Turtles") {
+            $('#snakes').hide();
+            $('#insects').hide();
+            $('#turtles').show();
 
-  });
+        } else if ($('input[name=animals-button]:checked').val() == "Snakes") {
+            $('#turtles').hide();
+            $('#insects').hide();
+            $('#snakes').show();
+
+        } else if ($('input[name=animals-button]:checked').val() == "Insects") {
+            $('#turtles').hide();
+            $('#snakes').hide();
+            $('#insects').show();
+
+        }
+    });
 });
